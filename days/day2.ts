57e5f00e0ee2af9aeb0a683846b9cd51bceecb1a
Jesse Buitenhuis <jessebuitenhuis@gmail.com>
2025-11-24 16:57:06 +0100

    Removed consle.log

--- days/day2.ts
+++ days/day2.ts
@@ -1,42 +1,43 @@
 import { readInputLines } from "../utils.js";
 
-const input = readInputLines("day2.txt").map(x => x.split("\t").map(x => parseInt(x, 10)));
+const input = readInputLines("day2.txt").map((x) =>
+  x.split("\t").map((x) => parseInt(x, 10)),
+);
 
 function calculateChecksum() {
-    let checksum = 0;
-    
-    for (const line of input) {
-        const min = Math.min(...line);
-        const max = Math.max(...line);
-        checksum += max - min;
-    }
-    
-    return checksum;
+  let checksum = 0;
+
+  for (const line of input) {
+    const min = Math.min(...line);
+    const max = Math.max(...line);
+    checksum += max - min;
+  }
+
+  return checksum;
 }
 
 function areEvenlyDivisible(a: number, b: number) {
-    return isEvenlyDivisible(a, b) || isEvenlyDivisible(b, a);
+  return isEvenlyDivisible(a, b) || isEvenlyDivisible(b, a);
 }
 
 function isEvenlyDivisible(a: number, b: number) {
-    const result = a / b;
-    if (result === Math.round(result)) return result;
-    return false;
+  const result = a / b;
+  if (result === Math.round(result)) return result;
+  return false;
 }
 
 function calculateDivisibleChecksum() {
-    return input.reduce((sum, line) => sum + getDivisibleForLine(line), 0);
+  return input.reduce((sum, line) => sum + getDivisibleForLine(line), 0);
 }
 
 function getDivisibleForLine(line: number[]): number {
-    for (let i = 0; i < line.length-1; i++) {
-        for (let j = i+1; j < line.length; j++) {
-            const result = areEvenlyDivisible(line[i]!, line[j]!);
-            console.log(line[i], line[j], result);
-            if (result) return result;
-        }
+  for (let i = 0; i < line.length - 1; i++) {
+    for (let j = i + 1; j < line.length; j++) {
+      const result = areEvenlyDivisible(line[i]!, line[j]!);
+      if (result) return result;
     }
-    throw new Error("No divisible found");
+  }
+  throw new Error("No divisible found");
 }
 
 console.log(calculateChecksum());
