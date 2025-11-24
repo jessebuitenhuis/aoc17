4f33d1aadf717c86dcfe78cc6a82da0fb570741f
Jesse Buitenhuis <jessebuitenhuis@gmail.com>
2025-11-24 14:40:57 +0000

    day1b

--- day1.ts
+++ day1.ts
@@ -3,13 +3,20 @@ import { readInput } from "./utils";
 const input = readInput("day1a.txt")
 console.log(input);
 
-let sum = 0;
-
-for (let i = 0; i < input.length; i++) {
-    const j = (i+1)%input.length;
-    if (input[i] == input[j]) {
-        sum += parseInt(input[i]);
+function calculateSum(offset = 1) {
+    let sum = 0;
+    for (let i = 0; i < input.length; i++) {
+        const j = (i+offset)%input.length;
+        if (input[i] == input[j]) {
+            sum += parseInt(input[i]);
+        }
     }
+    return sum;
 }
 
-console.log(sum);
+const answerA = calculateSum();
+const answerB = calculateSum(input.length / 2);
+
+
+console.log(answerA);
+console.log(answerB);
